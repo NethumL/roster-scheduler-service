e75eb9b808cd306e60dfcb0a2cdefb22f7023a12
Nethum Lamahewage <nethumpinsara@gmail.com>
2022-11-08 14:58:00 +0530

    Use secret for authentication

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, request
 
 from scheduler import Scheduler
@@ -7,6 +9,9 @@ app = Flask(__name__)
 
 @app.route("/", methods=["POST"])
 def get_schedule():
+    if os.getenv("SECRET") != request.headers.get("X-secret", ""):
+        return "", 401
+
     content_type = request.headers.get("Content-Type")
     if content_type == "application/json":
         data = request.json
